722113caf244b6f088f983dcc5defb4eaf7905c6
Hiram Ramirez <hialf.92@gmail.com>
2021-07-17 02:31:39 -0500

    Proyecto inicial de sfg-pet-clinic

---
+++ sfg-pet-clinic/src/test/java/mx/com/wiirux/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.wiirux.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
