d437adff3a349f9437e45174672e4ee8fc72b3a5
WiiruxCruz <hialf.92@gmail.com>
2021-08-05 14:42:45 -0500

    Se agrega listado de duenios funcional. Close #17

--- sfg-pet-clinic/pet-clinic-web/src/main/java/mx/com/wiirux/sfgpetclinic/controllers/DuenioController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/mx/com/wiirux/sfgpetclinic/controllers/DuenioController.java
@@ -1,14 +1,25 @@
 package mx.com.wiirux.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import mx.com.wiirux.sfgpetclinic.services.DuenioService;
 
 @RequestMapping("/duenios")
 @Controller
 public class DuenioController {
 	
+	private final DuenioService ds;
+	
+	public DuenioController(DuenioService ds) {
+		// TODO Auto-generated constructor stub
+		this.ds = ds;
+	}
+	
 	@RequestMapping({"","/","/index","/index.html"})
-	public String listaDuenios() {
+	public String listaDuenios(Model m) {
+		m.addAttribute("duenios", ds.findAll());
 		
 		return "duenios/index";
 	}
