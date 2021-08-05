de66b32f97124548284917cc0b69e581ef64f7e0
WiiruxCruz <hialf.92@gmail.com>
2021-08-05 15:29:03 -0500

    Ser agrega listado de veterinarios. Close #16

---
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/mx/com/wiirux/sfgpetclinic/controllers/VeterinarioController.java
@@ -0,0 +1,24 @@
+package mx.com.wiirux.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import mx.com.wiirux.sfgpetclinic.services.VeterinarioService;
+
+@Controller
+public class VeterinarioController {
+	private final VeterinarioService vs;
+	
+	public VeterinarioController(VeterinarioService vs) {
+		this.vs = vs;
+	}
+	
+	@RequestMapping({"/veterinarios","/veterinarios/index","/veterinarios/index.html"})
+	public String listaVeterinarios(Model m) {
+		
+		m.addAttribute("veterinarios", vs.findAll());
+		
+		return "veterinarios/index";
+	}
+}
