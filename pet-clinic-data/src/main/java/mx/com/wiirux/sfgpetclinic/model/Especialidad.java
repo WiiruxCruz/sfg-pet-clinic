5188e670ea9b2a7a1492e4b8397ee5d072d9870a
WiiruxCruz <hialf.92@gmail.com>
2021-08-14 23:22:48 -0500

    Se agregan Especialidad y se asocia con veterinario. Close #28

---
+++ pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/model/Especialidad.java
@@ -0,0 +1,13 @@
+package mx.com.wiirux.sfgpetclinic.model;
+
+public class Especialidad extends BaseEntity{
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
--- pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/model/Veterinario.java
+++ pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/model/Veterinario.java
@@ -1,5 +1,15 @@
 package mx.com.wiirux.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Veterinario extends Persona {
-	
+	private Set<Especialidad> especialidades;
+
+	public Set<Especialidad> getEspecialidades() {
+		return especialidades;
+	}
+
+	public void setEspecialidades(Set<Especialidad> especialidades) {
+		this.especialidades = especialidades;
+	}
 }
