5f584770d4700ccd03a9f70c2446233b40987519
WiiruxCruz <hialf.92@gmail.com>
2021-08-14 23:49:35 -0500

    Se agrega PetTypes to bootstrap. Close #33

--- pet-clinic-web/src/main/java/mx/com/wiirux/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mx/com/wiirux/sfgpetclinic/bootstrap/DataLoader.java
@@ -5,8 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import mx.com.wiirux.sfgpetclinic.model.Duenio;
+import mx.com.wiirux.sfgpetclinic.model.TipoMascota;
 import mx.com.wiirux.sfgpetclinic.model.Veterinario;
 import mx.com.wiirux.sfgpetclinic.services.DuenioService;
+import mx.com.wiirux.sfgpetclinic.services.TipoMascotaService;
 import mx.com.wiirux.sfgpetclinic.services.VeterinarioService;
 
 @Component
@@ -14,17 +16,28 @@ public class DataLoader implements CommandLineRunner{
 	
 	private final DuenioService ds;
 	private final VeterinarioService vs;
+	private final TipoMascotaService tms;
 	
 	
 	@Autowired
-	public DataLoader(DuenioService ds, VeterinarioService vs) {
+	public DataLoader(DuenioService ds, VeterinarioService vs, TipoMascotaService tms) {
 		this.ds = ds;
 		this.vs = vs;
+		this.tms = tms;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		TipoMascota perro = new TipoMascota();
+		perro.setTipo("perro");
+		TipoMascota savedDogPetTipoMascota = tms.save(perro);
+		
+		TipoMascota gato = new TipoMascota();
+		perro.setTipo("gato");
+		TipoMascota savedCatPetTipoMascota = tms.save(gato);
+		System.out.println("Mascotas cargadas...");
 		
 		Duenio d1 = new Duenio();
 		d1.setId(1L);
