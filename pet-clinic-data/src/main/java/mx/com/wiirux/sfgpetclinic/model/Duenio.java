1602787a5e19f664f99e4fca3f9d254accf1b963
WiiruxCruz <hialf.92@gmail.com>
2021-08-14 23:27:00 -0500

    Se agregan propiedades de contacto del duenio. Close #29

--- pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/model/Duenio.java
+++ pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/model/Duenio.java
@@ -3,7 +3,36 @@ package mx.com.wiirux.sfgpetclinic.model;
 import java.util.Set;
 
 public class Duenio extends Persona {
+	
+	private String direccion;
+	private String ciudad;
+	private String telefono;
+	
 	private Set<Mascota> mascotas;
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
 
 	public Set<Mascota> getMascotas() {
 		return mascotas;
@@ -12,4 +41,6 @@ public class Duenio extends Persona {
 	public void setMascotas(Set<Mascota> mascotas) {
 		this.mascotas = mascotas;
 	}
+	
+	
 }
