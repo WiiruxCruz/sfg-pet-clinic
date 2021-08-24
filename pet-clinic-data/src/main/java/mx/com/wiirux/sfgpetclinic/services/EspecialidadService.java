c68ac6ffa695d04985530fb5ee0d18801adf98e5
WiiruxCruz <hialf.92@gmail.com>
2021-08-23 23:50:17 -0500

    Se agrega Service Map de Especialidad. Close #32

---
+++ pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/services/EspecialidadService.java
@@ -0,0 +1,7 @@
+package mx.com.wiirux.sfgpetclinic.services;
+
+import mx.com.wiirux.sfgpetclinic.model.Especialidad;
+
+public interface EspecialidadService extends CrudService<Especialidad, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/services/map/EspecialidadServiceMap.java
@@ -0,0 +1,43 @@
+package mx.com.wiirux.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import mx.com.wiirux.sfgpetclinic.model.Especialidad;
+import mx.com.wiirux.sfgpetclinic.services.EspecialidadService;
+
+@Service
+public class EspecialidadServiceMap extends AbstractMapService<Especialidad, Long> implements EspecialidadService{
+
+	@Override
+	public Set<Especialidad> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Especialidad findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Especialidad save(Especialidad object) {
+		// TODO Auto-generated method stub
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Especialidad object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+	
+}
