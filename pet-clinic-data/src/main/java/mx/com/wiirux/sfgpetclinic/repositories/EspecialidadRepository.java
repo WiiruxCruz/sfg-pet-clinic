ca9140e197f52eb61126674cc074320c6fd61969
WiiruxCruz <hialf.92@gmail.com>
2021-08-30 22:49:04 -0500

    Se agregan los repositorios de las entidades. Close #40

---
+++ pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/repositories/EspecialidadRepository.java
@@ -0,0 +1,9 @@
+package mx.com.wiirux.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mx.com.wiirux.sfgpetclinic.model.Especialidad;
+
+public interface EspecialidadRepository extends CrudRepository<Especialidad, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/repositories/MascotaRepository.java
@@ -0,0 +1,9 @@
+package mx.com.wiirux.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mx.com.wiirux.sfgpetclinic.model.Mascota;
+
+public interface MascotaRepository extends CrudRepository<Mascota, Long>{
+
+}
