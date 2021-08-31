76d045be1ec6568070882fd60a71cd9d60a7160b
WiiruxCruz <hialf.92@gmail.com>
2021-08-30 21:57:38 -0500

    Se implementa baseEntity. Close #36

--- pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/mx/com/wiirux/sfgpetclinic/model/BaseEntity.java
@@ -2,7 +2,16 @@ package mx.com.wiirux.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
