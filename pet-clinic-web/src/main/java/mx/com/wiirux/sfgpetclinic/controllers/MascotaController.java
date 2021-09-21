4e29c101f483839b6ad831e1fe77e5f4f40404e2
WiiruxCruz <hialf.92@gmail.com>
2021-09-21 00:58:39 -0500

    Se agrega model atributo para Mascota Controlador #57

--- pet-clinic-web/src/main/java/mx/com/wiirux/sfgpetclinic/controllers/MascotaController.java
+++ pet-clinic-web/src/main/java/mx/com/wiirux/sfgpetclinic/controllers/MascotaController.java
@@ -1,13 +1,58 @@
 package mx.com.wiirux.sfgpetclinic.controllers;
 
+import java.util.Collection;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import mx.com.wiirux.sfgpetclinic.model.Duenio;
+import mx.com.wiirux.sfgpetclinic.model.TipoMascota;
+import mx.com.wiirux.sfgpetclinic.services.DuenioService;
+import mx.com.wiirux.sfgpetclinic.services.MascotaService;
+import mx.com.wiirux.sfgpetclinic.services.TipoMascotaService;
+
 @Controller
+@RequestMapping("/duenios/{duenioId}")
 public class MascotaController {
 	
+	private static final String VISTA_MASCOTA_CREAR_O_ACTUALIZAR_FORM = "mascotas/formularioCrearOActualizar";
+	private final MascotaService mascotaService;
+	private final DuenioService duenioService;
+	private final TipoMascotaService tipoMascotaService;
+	
+	public MascotaController(
+		MascotaService mascotaService,
+		DuenioService duenioService,
+		TipoMascotaService tipoMascotaService
+	) {
+		this.mascotaService = mascotaService;
+		this.duenioService = duenioService;
+		this.tipoMascotaService = tipoMascotaService;
+	}
+	
+	/*
 	@RequestMapping({"/mascotas", "/mascotas/index", "/mascotas/index.html"})
 	public String listaMascotas() {
 		return "mascotas/index";
+	}
+	*/
+	
+	@ModelAttribute("tiposMascota")
+	public Collection<TipoMascota> poblarTipoMascota(){
+		return this.tipoMascotaService.findAll();
+	}
+	
+	@ModelAttribute("duenios")
+	public Duenio buscarDuenio(@PathVariable("duenioId") Long duenioId) {
+		return this.duenioService.findById(duenioId);
+	}
+	
+	@InitBinder("duenio")
+	public void initduenioBinder(WebDataBinder dataBinder) {
+		dataBinder.setDisallowedFields("id");
 	}
 }
