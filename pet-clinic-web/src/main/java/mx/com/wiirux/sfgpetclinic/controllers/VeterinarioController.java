320d70691f033190a4e1682af4fcb11cb4b6c2cc
WiiruxCruz <hialf.92@gmail.com>
2021-10-06 01:31:15 -0500

    Se agrega json endpoint para veterinarios. Close #69

--- pet-clinic-web/src/main/java/mx/com/wiirux/sfgpetclinic/controllers/VeterinarioController.java
+++ pet-clinic-web/src/main/java/mx/com/wiirux/sfgpetclinic/controllers/VeterinarioController.java
@@ -1,9 +1,14 @@
 package mx.com.wiirux.sfgpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import mx.com.wiirux.sfgpetclinic.model.Veterinario;
 import mx.com.wiirux.sfgpetclinic.services.VeterinarioService;
 
 @Controller
@@ -20,5 +25,11 @@ public class VeterinarioController {
 		m.addAttribute("veterinarios", vs.findAll());
 		
 		return "veterinarios/index";
+	}
+	
+	@GetMapping("/api/veterinarios")
+	@ResponseBody
+	public Set<Veterinario> obtenerVeterinariosJson() {
+		return vs.findAll();
 	}
 }
